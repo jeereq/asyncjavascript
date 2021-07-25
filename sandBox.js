a31c12a2a2d9f40149a5f4178118efcf4c3c1ec8
jeereq <mingandajeereq@gmail.com>
2021-07-25 09:42:56 +0100

    add callback fonction

--- sandBox.js
+++ sandBox.js
@@ -1,14 +1,25 @@
 const request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange", () => {
-	try {
-		if (request.status === 404) throw "erreur";
-		if (request.readyState === 4 && request.status === 200)
-			console.log(request, request.responseText, request.readyState);
-	} catch (error) {
-		console.log(error);
-	}
-});
+const getWorks = (callback) => {
+	request.addEventListener("readystatechange", () => {
+		try {
+			if (request.readyState === 4 && request.status === 404)
+				throw {
+					status: request.status,
+					statusText: request.statusText
+				};
+			if (request.readyState === 4 && request.status === 200)
+				callback(null, request);
+		} catch (error) {
+			callback(error, null);
+		}
+	});
 
-request.open("get", "http://localhost:4000/works");
-request.send();
+	request.open("get", "http://localhost:4000/works");
+	request.send();
+};
+getWorks((error, data) => {
+	if (error) console.log(error);
+	else console.log(data);
+});
+ 
