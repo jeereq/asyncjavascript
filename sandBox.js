2a4730f13d98431e2db4efda2ca9f6ae7d3c3933
jeereq <mingandajeereq@gmail.com>
2021-07-25 10:00:51 +0100

    add json file and fetch into this  file

--- sandBox.js
+++ sandBox.js
@@ -9,17 +9,16 @@ const getWorks = (callback) => {
 					statusText: request.statusText
 				};
 			if (request.readyState === 4 && request.status === 200)
-				callback(null, request);
+				callback(null, JSON.parse(request.responseText));
 		} catch (error) {
 			callback(error, null);
 		}
 	});
 
-	request.open("get", "http://localhost:4000/works");
+	request.open("get", "data.json");
 	request.send();
 };
 getWorks((error, data) => {
 	if (error) console.log(error);
 	else console.log(data);
 });
- 
