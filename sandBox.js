ecad66b0ce937147c29720efd920f0a6f856e679
jeereq <mingandajeereq@gmail.com>
2021-07-25 10:40:01 +0100

    add function with Promise

--- sandBox.js
+++ sandBox.js
@@ -1,24 +1,45 @@
 const request = new XMLHttpRequest();
 
-const getWorks = (callback) => {
+const getWorks = (ressource, callback) => {
 	request.addEventListener("readystatechange", () => {
 		try {
 			if (request.readyState === 4 && request.status === 404)
 				throw {
 					status: request.status,
-					statusText: request.statusText
+					statusText: request.statusText,
+					data: null
 				};
-			if (request.readyState === 4 && request.status === 200)
-				callback(null, JSON.parse(request.responseText));
+			if (request.readyState === 4 && request.status === 200) {
+				const obj = {};
+				obj.status = 200;
+				obj.statusText = request.statusText;
+				obj.data = JSON.parse(request.responseText);
+				callback(null, obj);
+			}
 		} catch (error) {
 			callback(error, null);
 		}
 	});
 
-	request.open("get", "data.json");
+	request.open("get", ressource);
 	request.send();
 };
-getWorks((error, data) => {
+getWorks("data.jsons", (error, data) => {
 	if (error) console.log(error);
 	else console.log(data);
 });
+const getSomething = () => {
+	return new Promise((resolve, reject) => {
+		//	resolve("hello guys ");
+		reject("error ");
+	});
+};
+
+getSomething().then(
+	(data) => {
+		console.log(data);
+	},
+	(error) => {
+		console.log(error);
+	}
+);
